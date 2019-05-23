02361ced15bd91246115c59981dfb957c6152142
zhxymh <13552710076@126.com>
2019-05-23 23:49:22 +0800

    add log

--- src/AElf.OS.Consensus.DPos/DPoSAnnouncementReceivedEventDataHandler.cs
+++ src/AElf.OS.Consensus.DPos/DPoSAnnouncementReceivedEventDataHandler.cs
@@ -113,7 +113,7 @@ namespace AElf.OS.Consensus.DPos
                 var sureAmount = pubkeyList.Count.Mul(2).Div(3) + 1;
                 if (peersHadBlockAmount >= sureAmount)
                 {
-                    Logger.LogDebug($"LIB found in network layer: height {block.Key}");
+                    Logger.LogDebug($"LIB found in network layer: height {block.Key}, hash: {block.Value}");
                     return new BlockIndex(block.Value, block.Key);
                 }
             }
